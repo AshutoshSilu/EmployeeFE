ac42b8d3fe293ee169186b6c3b8424882f16bcc3
AshutoshSilu <ashutosh7004@gmail.com>
2025-11-13 20:47:36 +0530

    Remove backend changes

--- EmployeeBE/Employee/Repositories/IMenuRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Employee.Model;
-
-namespace Employee.Repositories
-{
-    public interface IMenuRepository
-    {
-        Task<List<MenuDetail>> GetMenuDetailsAsync(string? menuId, string? menuName, string? type, string? availability);
-    }
-}
